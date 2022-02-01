d055d16a94a4e9fb2b1d9ee2c4350992baaac21e
Shawn Penning <shawnpenning@amrock.com>
2022-01-31 21:20:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a length of the classroom:");
+decimal length = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Enter width of the classroom:");
+decimal width = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Area = {length * width} ");
+Console.WriteLine($"Perimeter = {length * 2 + width * 2} ");
