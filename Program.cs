52119bc8fd1f184888845a933a4e461e2d7f15e3
Shawn Penning <penningsp@hotmail.com>
2022-02-24 11:43:41 -0500

    I'm pretty sure you said we don't have to submit this lab, but I keep seeing a blank where my grade goes so I'm submitting it just to be sure. Also updated to include the bonuses.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter a length of the classroom:");
+Console.WriteLine("Enter a length of the room:");
 decimal length = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Enter width of the classroom:");
+Console.WriteLine("Enter width of the room:");
 decimal width = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine($"Area = {length * width} ");
+Console.WriteLine("Enter height of the room:");
+decimal height = Convert.ToDecimal(Console.ReadLine());
+decimal area = length * width;
+decimal volume = length * width * height;
+Console.WriteLine($"Area = {area} ");
 Console.WriteLine($"Perimeter = {length * 2 + width * 2} ");
+Console.WriteLine($"Volume = {volume} ");
+if (area <= 250) Console.WriteLine("Small Room");
+else if (area > 250 && area < 650) Console.WriteLine("Medium Room");
+else Console.WriteLine("Large Room");
+Console.WriteLine($"Surface Area = { length * width * 2 + length * height * 2 + width * height * 2 }");
